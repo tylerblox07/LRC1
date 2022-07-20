a2a67cd922d2000119d723f50aa80c4539fcc359
tylerblox07 <103872627+tylerblox07@users.noreply.github.com>
2022-07-19 22:20:48 -0500

    Update longroadhax.lua

--- longroadhax.lua
+++ longroadhax.lua
@@ -77,8 +77,8 @@ end)
 local FirstLabel = cPage.AddLabel("Citzen Cars")
 local FirstDropdown = cPage.AddDropdown("Spawn Car", {
     "commander",
-    "silver",
     "bison"
+
     }, function(Value)
         local args = {
             [1] = Value
